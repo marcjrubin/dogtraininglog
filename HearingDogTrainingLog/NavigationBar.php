3fe9fa62fe609fce6c3f174e4be7f742634ef30b
Marc J Rubin <20091921+mrubin78@users.noreply.github.com>
2017-09-26 22:53:53 -0400

    changes
    Changed in home page, links, etc

--- HearingDogTrainingLog/NavigationBar.php
+++ HearingDogTrainingLog/NavigationBar.php
@@ -10,15 +10,14 @@
             <div id="navBar">
                 
                 <header>
-                    <h1>Hearing Dog Training Log</h1>
+                    <h1>Training Log</h1>
                 </header> 
                 
                 <nav id="menu">
                   <a class="menuHover" href="CreateNewAccount.php">Register</a>
                   <a class="menuHover" href="Contact.php">Contact</a>
-                  <a class="menuHover" href="DogPark.php">Dog Park</a>
-                  <a class="menuHover" href="LogEntry.php">Log Entry</a>
                   <a class="menuHover" href="About.php">About</a>
+                  <a class="menuHover" href="Login.php">Login</a>
                   <a class="menuHover" href="index.php">Home</a>    
                 </nav>
                 
--- HearingDogTrainingLog/index.php
+++ HearingDogTrainingLog/index.php
@@ -10,89 +10,51 @@ and open the template in the editor.
         <title>Hearing Dog Training Log</title>
         <link href="https://fonts.googleapis.com/css?family=Maven+Pro" rel="stylesheet">
         <link href="assets/css/main.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/about.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/form.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/logentry.css" rel="stylesheet" type="text/css"/>
         <link href="assets/css/navigation.css" rel="stylesheet" type="text/css"/>
         <link href="assets/css/footer.css" rel="stylesheet" type="text/css"/>
-        <link href="assets/css/home.css" rel="stylesheet" type="text/css"/>
-        <link href="assets/css/loginform.css" rel="stylesheet" type="text/css"/>
-        <link href="assets/css/flexslider.css" rel="stylesheet" type="text/css"/>
-        <link href="assets/css/slides.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/parallax.css" rel="stylesheet" type="text/css"/>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script src="assets/js/scripts.js" type="text/javascript"></script>
-        <script src="assets/js/home.js" type="text/javascript"></script>
-        <script src="assets/js/about.js" type="text/javascript"></script>
-        <script src="assets/js/jquery.flexslider.js" type="text/javascript"></script>
+        <script src="assets/js/parallax.min.js" type="text/javascript"></script>
     </head>
     <body>
-        <div id="navHeader" class="navHome">
-            <div id="navBar">
-                
-                <header>
-                    <h1>Hearing Dog Training Log</h1>
-                </header>
-                
-                <nav>
-                  <a id="login" href="#">Login</a>                     
-                </nav>  
-                
-                <div id="loginbox">  
-                    <div id="loginform">
-                        <h2>Login</h2>
-                        <form>
-                          <input type="text" id="username" name="username" placeholder="username" size="30">
-
-                          <br> 
-
-                          <input type="password" id="pwd" name="pwd" placeholder="password" size="30">
-
-                          <br> 
-
-                          <input class="styleButton" type="submit" value="Login">
-                        </form>
-                    </div>
-                </div>
-                
-            <script src="assets/js/login.js" type="text/javascript"></script>
-                
-            </div> 
-         </div>
+        <?php 
+            include 'NavigationBar.php';
+        ?>
         
-        <div id="backgroundImage">
-            <header>
-            </header>
-        </div>
+        <div class="parallax-container" data-parallax="scroll" data-image-src="./assets/img/GraduateInGarden.jpg"></div>
         
-        <hr>
-        
-        <div>
-            <h1>Flexslider</h1>    
-            <div class="flexslider">
-                <ul class="slides">
-                    <li><img src="assets/img/nature1.jpg" /></li>
-                    <li><img src="assets/img/nature2.jpg" /></li>
-                    <li><img src="assets/img/nature3.jpg" /></li>
-                    <li><img src="assets/img/nature4.jpg" /></li>
-                    <li><img src="assets/img/nature5.jpg" /></li>
-                </ul>
+        <section class="content-container">
+            <div class="content-body">
+                <h1>Login</h1>
+                <p>
+                    <a href="#" class="btn btn-primary btn-lg active">Login</a><br>
+                    Login to enter your training log
+                </p>
             </div>
-            
-            <script type="text/javascript">
-                $(function() {
-                   $('.flexslider').flexslider({
-                       slideshow: true,
-                       startAt: 0,
-                       slideshowSpeed: 2500,
-                       initDelay: 0,
-                       randomize: true,
-                       animation: "slide"
-                   }); 
-                });
-            </script>
-            
-        </div>        
+        </section>
         
-        <hr>
+        <div class="parallax-container2" data-parallax="scroll" data-image-src="./assets/img/LabsChasingBalls.jpg"></div> 
         
-        <main>
+        <section class="content-container">
+            <div class="content-body">
+                <h1>About</h1>
+                <p>
+                    A place where dog trainers and handlers log the training sessions. 
+                </p>
+            </div>
+        </section>
+        
+             
+                
+        <script type="text/javascript">
+            $('.parallax-container').parallax({imageSrc: '/assets/img/GraduateInGarden.jpg'});
+            $('.parallax-container2').parallax({imageSrc: '/assets/img/LabsChasingBalls.jpg'});
+        </script>
+        
+<!--        <main>
             <a href="About.php"><section id="about" class="topRow running" onmouseover="textItalics(this)" onmouseout="textNormal(this)">
                     <h2>About</h2>                
                 </section></a>
@@ -108,8 +70,8 @@ and open the template in the editor.
             <a href="Contact.php"><section id="contact"  class="bottomRow garden" onmouseover="textItalics(this)" onmouseout="textNormal(this)">
                 <h2>Contact</h2>
             </section></a>
-        </main>
-        
+        </main>-->
+
         <?php 
             include 'Footer.php';
         ?>
